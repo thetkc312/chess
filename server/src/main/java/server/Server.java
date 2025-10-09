3db029ec87c42d39d36e25276785a7a4bd609e32
thetkc312 <thetkc312@gmail.com>
2025-10-09 10:36:42 -0600

    dummy implementation of post and delete for the server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,41 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin javalinServer;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalinServer = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        // TODO: Implement actual delete functionality, see where to do this
+        javalinServer.post("user", ctx -> register(ctx));
+        javalinServer.delete("db", ctx -> ctx.result("{}"));
 
+    }
+
+    private void register(Context ctx) {
+        Gson serializer = new Gson();
+        String requestJson = ctx.body();
+        Map request = serializer.fromJson(requestJson, Map.class);
+
+        // FIXME: Implement actual authtoken generating
+        Map result = Map.of("username", request.get("username"), "authToken", "myHardCodedAuthToken");
+        ctx.result(serializer.toJson(result));
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        javalinServer.start(desiredPort);
+        return javalinServer.port();
     }
 
     public void stop() {
-        javalin.stop();
+        javalinServer.stop();
     }
 }
