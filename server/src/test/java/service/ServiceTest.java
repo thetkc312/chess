c43a25b28c32f35ccc211e3a6277675dd365abbb
thetkc312 <thetkc312@gmail.com>
2025-10-26 03:35:29 -0700

    Implementation of Service Tests, removal of incomplete DataAccessTest

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -5,12 +5,14 @@ import dataaccess.*;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import passoff.model.TestAuthResult;
 
 import javax.xml.crypto.Data;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +21,7 @@ public class ServiceTest {
 
     private static UserData userBob;
     private static UserData userBot;
+    private static UserData userBad;
     private static String gameName;
     private DataAccess dataAccess;
     private Service service;
@@ -27,6 +30,7 @@ public class ServiceTest {
     public static void setup() {
         userBob = new UserData("Bob", "bob@bob.bob", "bigboybob");
         userBot = new UserData("Bot", "bot@bot.bot", "bigboybot");
+        userBad = new UserData("Bad", "bad@bad.bad", "");
     }
 
     @BeforeEach
@@ -38,62 +42,143 @@ public class ServiceTest {
     }
 
     @Test
-    void registerValid() {
+    void registerValid() throws BadRequestException, AlreadyTakenException, DataAccessException {
+        AuthData authDataBob = service.register(userBob);
+        assertNotNull(authDataBob);
+        assertTrue(dataAccess.userExists(userBob.username()));
+        assertEquals(userBob.username(), authDataBob.username());
+        assertTrue(dataAccess.authExists(authDataBob.authToken()));
 
-    }
-    @Test
-    void registerInvalid() {
-        UserData user;
-    }
+        AuthData authDataBot = service.register(userBot);
+        assertNotNull(authDataBot);
+        assertTrue(dataAccess.userExists(userBot.username()));
+        assertEquals(userBot.username(), authDataBot.username());
+        assertTrue(dataAccess.authExists(authDataBot.authToken()));
 
-    @Test
-    void loginValid() {
-        UserData user;
+        assertNotEquals(authDataBob.username(), authDataBot.username());
+        assertNotEquals(authDataBob.authToken(), authDataBot.authToken());
     }
     @Test
-    void loginInvalid(UserData user)
-            throws BadRequestException, InvalidCredentialsException, DataAccessException {
-    }
-
-    @Test
-    void logoutValid(String authToken)
-            throws InvalidCredentialsException, DataAccessException {
-    }
-    @Test
-    void logoutInvalid(String authToken)
-            throws InvalidCredentialsException, DataAccessException {
+    void registerInvalid() throws BadRequestException, AlreadyTakenException, DataAccessException {
+        assertThrows(BadRequestException.class, () -> service.register(userBad));
+        assertFalse(dataAccess.userExists(userBad.username()));
+        service.register(userBob);
+        assertThrows(AlreadyTakenException.class, () -> service.register(userBob));
     }
 
     @Test
-    void listValid(String authToken)
-            throws InvalidCredentialsException, DataAccessException {
+    void loginValid() throws BadRequestException, InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = service.login(userBob);
+        assertNotNull(authData);
+        assertNotNull(authData.username());
+        assertNotNull(authData.authToken());
+        assertEquals(userBob.username(), authData.username());
+        assertTrue(dataAccess.userExists(userBob.username()));
+        assertTrue(dataAccess.authExists(authData.authToken()));
+        assertTrue(dataAccess.validLogin(userBob.username(), userBob.password()));
+
+        AuthData authData1 = service.login(userBob);
+        assertNotNull(authData1);
+        assertNotNull(authData1.username());
+        assertNotNull(authData1.authToken());
+        assertEquals(userBob.username(), authData1.username());
+        assertTrue(dataAccess.authExists(authData1.authToken()));
+
+        assertNotEquals(authData.authToken(), authData1.authToken());
+        assertEquals(authData.username(), authData1.username());
+
     }
     @Test
-    void listInvalid(String authToken)
-            throws InvalidCredentialsException, DataAccessException {
+    void loginInvalid() throws BadRequestException, InvalidCredentialsException, DataAccessException {
+        assertThrows(BadRequestException.class, () -> service.login(userBad));
+        dataAccess.createUser(userBob);
+        UserData fakeUser = new UserData(userBob.username(), userBob.email(), userBob.password().concat("Wrong"));
+        assertThrows(InvalidCredentialsException.class, () -> service.login(fakeUser));
     }
 
     @Test
-    void createValid(String authToken, String gameName)
-            throws BadRequestException, InvalidCredentialsException, DataAccessException {
+    void logoutValid() throws InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+        dataAccess.createUser(userBot);
+        AuthData authData1 = dataAccess.createAuth(userBot.username());
+        service.logout(authData.authToken());
+        assertTrue(dataAccess.userExists(userBob.username()));
+        assertFalse(dataAccess.authExists(authData.authToken()));
+        assertTrue(dataAccess.authExists(authData1.authToken()));
     }
     @Test
-    void createInvalid(String authToken, String gameName)
-            throws BadRequestException, InvalidCredentialsException, DataAccessException {
+    void logoutInvalid() throws InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+
+        assertThrows(InvalidCredentialsException.class, () -> service.logout(authData.authToken().concat("Wrong")));
+        assertTrue(dataAccess.userExists(userBob.username()));
+        assertTrue(dataAccess.authExists(authData.authToken()));
     }
 
     @Test
-    void joinGameValid(String authToken, int gameID, ChessGame.TeamColor teamColor)
-            throws BadRequestException, InvalidCredentialsException, AlreadyTakenException, DataAccessException {
+    void listValid() throws InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData bobAuth = dataAccess.createAuth(userBob.username());
+        assertEquals(0, service.list(bobAuth.authToken()).size());
+
+        int gameID = dataAccess.createGame(userBob.username());
+        ArrayList<GameData> listResponse = service.list(bobAuth.authToken());
+        assertEquals(1, listResponse.size());
+        assertEquals(gameID, listResponse.getFirst().gameID());
     }
     @Test
-    void joinGameInvalid(String authToken, int gameID, ChessGame.TeamColor teamColor)
-            throws BadRequestException, InvalidCredentialsException, AlreadyTakenException, DataAccessException {
+    void listInvalid() throws InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+
+        assertThrows(InvalidCredentialsException.class, () -> service.list(authData.authToken().concat("Wrong")));
+
     }
 
     @Test
-    void clearValid()
-            throws DataAccessException {
+    void createValid() throws BadRequestException, InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+        int gameID = service.create(authData.authToken(), userBob.username().concat("Game"));
+        assertTrue(dataAccess.gameExists(gameID));
+    }
+    @Test
+    void createInvalid() throws BadRequestException, InvalidCredentialsException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+        assertThrows(BadRequestException.class, () -> service.create(authData.authToken(), ""));
+        assertThrows(InvalidCredentialsException.class, () -> service.create(authData.authToken().concat("Wrong"), userBob.username().concat("Game")));
+        assertEquals(0, dataAccess.listGames().size());
+    }
+
+    @Test
+    void joinGameValid() throws BadRequestException, InvalidCredentialsException, AlreadyTakenException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+        int gameID = dataAccess.createGame(userBob.username().concat("Game"));
+        service.joinGame(authData.authToken(), gameID, ChessGame.TeamColor.WHITE);
+        assertEquals(userBob.username(), dataAccess.listGames().getFirst().whiteUsername());
+        assertNull(dataAccess.listGames().getFirst().blackUsername());
+        service.joinGame(authData.authToken(), gameID, ChessGame.TeamColor.BLACK);
+        assertEquals(userBob.username(), dataAccess.listGames().getFirst().blackUsername());
+    }
+    @Test
+    void joinGameInvalid() throws BadRequestException, InvalidCredentialsException, AlreadyTakenException, DataAccessException {
+        dataAccess.createUser(userBob);
+        AuthData authData = dataAccess.createAuth(userBob.username());
+        int gameID = dataAccess.createGame(userBob.username().concat("Game"));
+        assertThrows(BadRequestException.class, () -> service.joinGame(authData.authToken(), 0, ChessGame.TeamColor.WHITE));
+        assertThrows(BadRequestException.class, () -> service.joinGame(authData.authToken(), gameID, null));
+        assertTrue(dataAccess.roleOpen(gameID, ChessGame.TeamColor.WHITE));
+        assertThrows(InvalidCredentialsException.class, () -> service.joinGame(authData.authToken().concat("Wrong"), gameID, ChessGame.TeamColor.WHITE));
+        assertTrue(dataAccess.roleOpen(gameID, ChessGame.TeamColor.WHITE));
+    }
+
+    @Test
+    void clearValid() throws DataAccessException {
         dataAccess.createUser(userBob);
         AuthData authData = dataAccess.createAuth(userBob.username());
         int gameID = dataAccess.createGame(gameName);
