576d6c4bb9691c9f9a5c859efc81d6a0e550ca91
Paul Atreides <thetkc312@gmail.com>
2025-11-14 13:14:05 -0700

    Addition of skeleton structure for ServerFacadeTests

--- client/src/main/test/java/client/ServerFacadeTests.java
+++ client/src/main/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,23 @@
 package client;
 
 import org.junit.jupiter.api.*;
+import server.ResponseException;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        int port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String serverURL = "http://localhost:" + port;
+        serverFacade = new ServerFacade(serverURL);
     }
 
     @AfterAll
@@ -20,10 +25,87 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    // clear()
+    @Test
+    public void clearPositive() throws ResponseException {
+
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void clearNegative() throws ResponseException {
+
+    }
+
+
+    // register()
+    @Test
+    public void registerPositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void registerNegative() throws ResponseException {
+
+    }
+
+
+    // login()
+    @Test
+    public void loginPositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void loginNegative() throws ResponseException {
+
+    }
+
+
+    // logout()
+    @Test
+    public void logoutPositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void logoutNegative() throws ResponseException {
+
+    }
+
+
+    // listGames()
+    @Test
+    public void listGamesPositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void listGamesNegative() throws ResponseException {
+
+    }
+
+
+    // createGame()
+    @Test
+    public void createGamePositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void createGameNegative() throws ResponseException {
+
+    }
+
+
+    // joinGame()
+    @Test
+    public void joinGamePositive() throws ResponseException {
+
+    }
+
+    @Test
+    public void joinGameNegative() throws ResponseException {
+
     }
 
 }
