6b3e74c4360b0dbe4c77aae940f4f4ec504e1b01
Paul Atreides <thetkc312@gmail.com>
2025-12-07 00:41:46 -0700

    Implementation of proper session opening, closing and handling of CONNECT messages.

---
+++ server/src/main/java/server/websocket/UserRole.java
@@ -0,0 +1,7 @@
+package server.websocket;
+
+public enum UserRole {
+    WHITE,
+    BLACK,
+    OBSERVER
+}
--- server/src/main/java/server/websocket/UserWSHandler.java
+++ server/src/main/java/server/websocket/UserWSHandler.java
@@ -6,19 +6,27 @@ import com.google.gson.Gson;
 import dataaccess.DataAccess;
 import dataaccess.DatabaseException;
 import io.javalin.websocket.*;
+import model.GameData;
 import websocket.commands.UserGameCommand;
 import websocket.commands.UserMoveCommand;
+import websocket.messages.ServerLoadGameMessage;
+import websocket.messages.ServerMessage;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class UserWSHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     // SessionID : WsContext
-    public final ConcurrentHashMap<String, WsContext> wsSessions = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, WsContext> wsSessions = new ConcurrentHashMap<>();
+
+    // SessionID : Username
+    private final ConcurrentHashMap<String, String> sessionUsernames = new ConcurrentHashMap<>();
 
     // GameID : [ SessionID, SessionID, ... ]
-    public final ConcurrentHashMap<String, String[]> gameSessions = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, ArrayList<String>> gameSessions = new ConcurrentHashMap<>();
 
     public final DataAccess dataAccess;
 
@@ -29,16 +37,21 @@ public class UserWSHandler implements WsConnectHandler, WsMessageHandler, WsClos
 
     @Override
     public void handleConnect(WsConnectContext ctxConnect) {
+        System.out.println("Establishing WebSocket Connection Session:");
+        System.out.println(ctxConnect.sessionId());
         // TODO: Add this session to be tracked
         ctxConnect.enableAutomaticPings();
         wsSessions.put(ctxConnect.sessionId(), ctxConnect);
-        System.out.println("Establishing WebSocket Connection Session:");
-        System.out.println(ctxConnect.sessionId());
     }
 
     @Override
     public void handleClose(WsCloseContext ctxClose) {
-        wsSessions.remove(ctxClose.sessionId());
+        String closingSessionID = ctxClose.sessionId();
+        wsSessions.remove(closingSessionID);
+        sessionUsernames.remove(closingSessionID);
+        for (ArrayList<String> gameParticipants : gameSessions.values()) {
+            gameParticipants.remove(closingSessionID);
+        }
         System.out.println("Closed session " + ctxClose.sessionId());
     }
 
@@ -51,28 +64,48 @@ public class UserWSHandler implements WsConnectHandler, WsMessageHandler, WsClos
         UserGameCommand userGameCommand = serializer.fromJson(wsMessageJson, UserGameCommand.class);
 
         if (!validCommandCredentials(userGameCommand)) {
-            ServerCommandSender.sendError(ctxMessage, "The credentials for this command are invalid:\n" + userGameCommand);
+            ServerCommandSender.sendError(ctxMessage, "There was an error processing the credentials for this command:\n" + userGameCommand);
+            return;
         }
 
-        int gameID = userGameCommand.getGameID();
-        String username = getUsername(userGameCommand.getAuthToken());
-        try {
-            switch (userGameCommand.getCommandType()) {
-                case CONNECT -> processConnect(wsSessionID, gameID);
-                case MAKE_MOVE -> processMakeMove(wsSessionID, gameID, serializer.fromJson(wsMessageJson, UserMoveCommand.class).getMove());
-                case LEAVE -> processLeave(wsSessionID, gameID);
-                case RESIGN -> processResign(wsSessionID, gameID);
-            }
-        } catch (NullPointerException e) {
-            ServerCommandSender.sendError(ctxMessage, "Could not recognize UserGameCommand type");
+        if (userGameCommand.getCommandType() == null) {
+            ServerCommandSender.sendError(ctxMessage, "There was an error processing the UserGameCommand type");
+            return;
+        }
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> processConnect(wsSessionID, userGameCommand);
+            case MAKE_MOVE -> processMakeMove(wsSessionID, userGameCommand, serializer.fromJson(wsMessageJson, UserMoveCommand.class).getMove());
+            case LEAVE -> processLeave(wsSessionID, userGameCommand);
+            case RESIGN -> processResign(wsSessionID, userGameCommand);
         }
     }
 
     private boolean validCommandCredentials(UserGameCommand userGameCommand) {
         //TODO: Implement credential checking
         System.out.println("Checking UserGameCommand credentials:");
+
         System.out.println(userGameCommand.getGameID());
+        try {
+            ArrayList<GameData> gameDataArrayList = dataAccess.listGames();
+            ArrayList<Integer> validGameIDList = new ArrayList<>();
+            for (GameData gameData : gameDataArrayList) {
+                validGameIDList.add(gameData.gameID());
+            }
+            if (!validGameIDList.contains(userGameCommand.getGameID())) {
+                return false;
+            }
+        } catch (DatabaseException e) {
+            return false;
+        }
+
         System.out.println(userGameCommand.getAuthToken());
+        try {
+            if (!dataAccess.authExists(userGameCommand.getAuthToken())) {
+                return false;
+            }
+        } catch (DatabaseException e) {
+            return false;
+        }
         return true;
     }
 
@@ -81,29 +114,87 @@ public class UserWSHandler implements WsConnectHandler, WsMessageHandler, WsClos
         return "fakeUsername";
     }
 
-    private void processConnect(String wsSessionID, int gameID) {
-        //TODO: Implement connection processing
+    private void processConnect(String rootSessionID, UserGameCommand userGameCommand) {
         System.out.println("Processing CONNECT message...");
+        WsContext rootUserSession = wsSessions.get(rootSessionID);
+        // If this sessionID is already stored, then the client is redundantly trying to connect a second time
+        if (sessionUsernames.contains(rootSessionID)) {
+            ServerCommandSender.sendError(rootUserSession, "An error was caused as a CONNECT request was made to the server while this session is already connected.");
+            return;
+        } // else
+        String rootUsername = getUsername(userGameCommand.getAuthToken());
+        sessionUsernames.put(rootSessionID, rootUsername);
 
+        // If the game the client is connecting to is already documented, simply add its sessionID to the String Array for that game
+        int activeGameID = userGameCommand.getGameID();
+        if (gameSessions.containsKey(activeGameID)) {
+            gameSessions.get(activeGameID).add(rootSessionID);
+        // If the game the client is connecting to has not been documented, add it to the set by its activeGameID with this user as the only element of its String Array value
+        } else {
+            ArrayList<String> newGameSessionIDArrayList = new ArrayList<>();
+            newGameSessionIDArrayList.add(rootSessionID);
+            gameSessions.put(activeGameID, newGameSessionIDArrayList);
+        }
+
+        try {
+            ArrayList<GameData> gameDataArrayList = dataAccess.listGames();
+            // Find which game this root user is in
+            for (GameData gameData : gameDataArrayList) {
+                if (gameData.gameID() == activeGameID) {
+                    // When root client sends CONNECT, server sends LOAD_GAME back to that root client
+                    ServerCommandSender.sendLoadGame(rootUserSession, gameData);
+
+                    ArrayList<String> allGameSessionIDs = gameSessions.get(gameData.gameID());
+                    String connectNotification = String.format("User %s has joined the game as ", rootUsername);
+                    connectNotification += switch (findUserRole(rootUsername, gameData)) {
+                        case WHITE -> "the White Player.";
+                        case BLACK -> "the Black Player.";
+                        case OBSERVER -> "an observer.";
+                    };
+                    for (String sessionID : allGameSessionIDs) {
+                        if (!sessionID.equals(rootSessionID)) {
+                            // Also send a NOTIFICATION ServerMessage to all other participants in this game
+                            ServerCommandSender.sendNotification(wsSessions.get(sessionID), connectNotification);
+                        }
+                    }
+                }
+            }
+        } catch (DatabaseException e) {
+            ServerCommandSender.sendError(rootUserSession, "There was an error connecting you to your game.");
+            return;
+        }
     }
 
-    private void processMakeMove(String wsSessionID, int gameID, ChessMove move) {
+    private UserRole findUserRole(String username, GameData gameData) {
+        // WARNING: This method fails when joins a game as white, then also as black.
+        // It will notify all other users that the user has joined as white both times.
+        if (username.equals(gameData.whiteUsername())) {
+            return UserRole.WHITE;
+        }
+        if (username.equals(gameData.blackUsername())) {
+            return UserRole.BLACK;
+        }
+        return UserRole.OBSERVER;
+    }
+
+    private void processMakeMove(String wsSessionID, UserGameCommand userGameCommand, ChessMove move) {
         System.out.println("Processing MAKE_MOVE message...");
         //TODO: Implement make_move processing
         try {
             dataAccess.updateGameBoard(new ChessBoard());
         } catch (DatabaseException e) {
             ServerCommandSender.sendError(wsSessions.get(wsSessionID), "There was an error processing your move.");
+            return;
         }
     }
 
-    private void processLeave(String wsSessionID, int gameID) {
+    private void processLeave(String wsSessionID, UserGameCommand userGameCommand) {
         //TODO: Implement leave processing
         System.out.println("Processing LEAVE message...");
 
     }
 
-    private void processResign(String wsSessionID, int gameID) {
+    private void processResign(String wsSessionID, UserGameCommand userGameCommand) {
         //TODO: Implement resign processing
         System.out.println("Processing RESIGN message...");
 
