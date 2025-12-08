56fb2aa78432b8233825de839996fc916d8d6abc
Paul Atreides <thetkc312@gmail.com>
2025-12-08 16:39:57 -0700

    Added method to GameListResponse to find a GameData object from a provided gameID

--- shared/src/main/java/endpointresponses/GameListResponse.java
+++ shared/src/main/java/endpointresponses/GameListResponse.java
@@ -4,4 +4,13 @@ import model.GameData;
 import java.util.ArrayList;
 
 public record GameListResponse (ArrayList<GameData> games) {
+
+    public GameData findGameData(int gameID) {
+        for (GameData gameData : games) {
+            if (gameData.gameID() == gameID) {
+                return gameData;
+            }
+        }
+        return null;
+    }
 }
